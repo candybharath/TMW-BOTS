b7142196ad8fdc0383e303f29ab5e19427e6b26a
TORZANMOVIE <129737179+TORZANMOVIE@users.noreply.github.com>
2023-04-30 18:11:31 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Nivedkc1001/DQ-the-file-donor
+  git clone https://github.com/TORZANMOVIE/SHARKAUTOFILTERBOT
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /DQ-The-File-Donor
+  git clone $UPSTREAM_REPO /SHARKFILTERBOT
 fi
 cd /DQ-The-File-Donor
 pip3 install -U -r requirements.txt
-echo "Starting DQ-The-File-Donor...."
+echo "Starting SHARKFILTERBOT...."
 python3 bot.py
