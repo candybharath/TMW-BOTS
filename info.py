3c5d5abc232f54c61df5010bde35d873edb781ad
TORZANMOVIE <129737179+TORZANMOVIE@users.noreply.github.com>
2023-05-06 10:43:17 +0530

    Update info.py

--- info.py
+++ info.py
@@ -47,10 +47,10 @@ DATABASE_NAME = environ.get('DATABASE_NAME', "SHARK")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Others
-IS_VERIFY = bool(environ.get('VERIFY', False))
+IS_VERIFY = bool(environ.get('IS_VERIFY', False))
 SHORTLINK_URL = environ.get('SHORTLINK_URL', '')
 SHORTLINK_API = environ.get('SHORTLINK_API', '')
-IS_SHORTLINK = bool(environ.get('IS_SHORTLINK', True))
+IS_SHORTLINK = bool(environ.get('IS_SHORTLINK', False))
 DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in environ.get('DELETE_CHANNELS', '0').split()]
 MAX_B_TN = environ.get("MAX_B_TN", "5")
 MAX_BTN = is_enabled((environ.get('MAX_BTN', "True")), True)
