206a69c5b6da1e5bc09975ce80a96b64ed051c71
candybharath <126889025+candybharath@users.noreply.github.com>
2023-06-17 20:58:21 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -333,14 +333,7 @@ async def start(client, message):
     if f_caption is None:
         f_caption = f"{files.file_name}"
     
-        btn = [[
-            InlineKeyboardButton("Vᴇʀɪғʏ", url=await get_token(client, message.from_user.id, f"https://telegram.me/{temp.U_NAME}?start=", file_id))
-        ]]
-        await message.reply_text(
-            text="<b>Yᴏᴜ ᴀʀᴇ ɴᴏᴛ ᴠᴇʀɪғɪᴇᴅ!\nKɪɴᴅʟʏ ᴠᴇʀɪғʏ ᴛᴏ ᴄᴏɴᴛɪɴᴜᴇ Sᴏ ᴛʜᴀᴛ ʏᴏᴜ ᴄᴀɴ ɢᴇᴛ ᴀᴄᴄᴇss ᴛᴏ ᴜɴʟɪᴍɪᴛᴇᴅ ᴍᴏᴠɪᴇs ᴜɴᴛɪʟ 12 ʜᴏᴜʀs ғʀᴏᴍ ɴᴏᴡ !</b>",
-            protect_content=True,
-            reply_markup=InlineKeyboardMarkup(btn)
-        )
+        
         return
     await client.send_cached_media(
         chat_id=message.from_user.id,
